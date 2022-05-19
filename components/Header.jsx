13d11fd075f0646fc7cebbc00156a88bfefbbd17
Jattin Manhas <jattinmanhas0102@gmail.com>
2022-05-19 11:35:53 +0530

    Updated Some Design

--- components/Header.jsx
+++ components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
             <div className="md:float-left block">
                 <Link href="/">
                     <span className='cursor-pointer font-bold text-2xl text-white'>
-                        Graph CMS
+                        Jattin's Blog
                     </span>
                 </Link>
             </div>
