ce355f933d94d5fc9b96b481ceedc4fe92065093
Jattin Manhas <jattinmanhas0102@gmail.com>
2022-05-19 11:10:15 +0530

    Added More Functionality to website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,46 @@
 // import type { NextPage } from 'next'
 import Head from 'next/head'
-import {PostCard, Categories, PostWidget} from "../components"
-import {getPosts} from "../services"
+import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
+import { FeaturedPosts } from '../sections'
 
 // const posts = [
 //   {title: 'React Testing', excerpt: "Learn React Testing"},
 //   {title: 'React with Tailwind', excerpt: "Learn React with Tailwind"}
 // ]
 
-export default function Home({posts}){
+export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto mb-8 px-10">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
-        {posts.map((post)=> <PostCard post={post.node} key={post.title}/>
-        )}
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post) => (
+            <PostCard post={post.node} key={post.title} />
+          ))}
         </div>
-        
-      <div className='lg:col-span-4 col-span-1'>
-          <div className='lg: sticky relative top-8'>
-            <PostWidget/>
-            <Categories/>
+
+        <div className="col-span-1 lg:col-span-4">
+          <div className="lg: relative sticky top-8">
+            <PostWidget />
+            <Categories />
           </div>
+        </div>
       </div>
-    </div>
     </div>
   )
 }
 
-export async function getStaticProps(){
-  const posts = (await getPosts() || []);
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
 
-  return{
-    props: {posts}
+  return {
+    props: { 
+      posts 
+    },
   }
 }
